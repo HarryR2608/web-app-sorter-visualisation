f30b260249d7fd39ef262636ed4651a3fab99dee
HarryR2608 <43859223+HarryR2608@users.noreply.github.com>
2020-09-02 20:26:30 +0100

    Create progress bar component

---
+++ src/components/body/progressBar/ProgressBar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./ProgressBar.css";
+
+const ProgressBar = () => {
+  return (
+    <div className="progressBar">
+      <div className="progressBar_active" />
+    </div>
+  );
+};
+
+export default ProgressBar;
