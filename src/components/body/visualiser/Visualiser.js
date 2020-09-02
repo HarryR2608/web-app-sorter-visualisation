5d5c4616437b45626aaf41ff5d7a5994c52b302b
HarryR2608 <43859223+HarryR2608@users.noreply.github.com>
2020-09-02 20:27:04 +0100

    Create visualiser component

---
+++ src/components/body/visualiser/Visualiser.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Visualiser.css";
+import Bar from "./bar/Bar";
+
+const Visualiser = () => {
+  return (
+    <div className="visualiser">
+      <Bar />
+      <Bar />
+      <Bar />
+      <Bar />
+      <Bar />
+    </div>
+  );
+};
+
+export default Visualiser;
