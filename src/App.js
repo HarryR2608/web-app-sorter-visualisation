19fddcc6ae0666b23045fa8573d22fb388ed769f
HarryR2608 <43859223+HarryR2608@users.noreply.github.com>
2020-09-02 20:27:35 +0100

    Fix git issues

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { Component } from "react";
+import Header from "./components/header/Header";
+import Body from "./components/body/Body";
 
-function App() {
-  return <div className="App"></div>;
+class App extends Component {
+  state = {
+    algorithm: null,
+  };
+
+  handleAlgorithmChange = (e) => {
+    this.setState({ algorithm: e });
+  };
+
+  handleRandomise = () => {
+    console.log(this.state.algorithm);
+  };
+
+  render() {
+    const { algorithm } = this.state;
+    return (
+      <div className="app">
+        <Header
+          handleAlgorithmChange={this.handleAlgorithmChange}
+          handleRandomise={this.handleRandomise}
+          algorithm={algorithm}
+        />
+        <Body />
+      </div>
+    );
+  }
 }
 
 export default App;
